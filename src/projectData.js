9b72fa02e3b003cc383de2af4fc82765f824dfea
Marjorie <marjoriebaut.w@gmail.com>
2023-06-29 17:29:44 -0400

    a

--- src/projectData.js
+++ src/projectData.js
@@ -1,23 +1,18 @@
 
 const projectData = [
-    {
-        name: 'E-clinic',
-        description: 'This application will use an Express.js back end and will save and retrieve note data from a JSON file.',
-        link: 'https://eclinicproject2.herokuapp.com/',
-        GitHub: 'https://github.com/marjbaut/clinicManagement',
-    },
-    // {
-    //     name: 'NoteTaker',
-    //     description: 'This website can be used to organize patient and doctor records in hopitals or clinics.',
-    //     link: 'https://note-taker-mb.herokuapp.com/notes',
-    //     GitHub: 'proj2'
-    // },
+ 
     {
         name: 'Appointment Booking System',
-        description: 'A software solution and reservation system that makes it simple for users to book an appointment.Can be use for any kind of bussines such as clinics, salon services and any other industry.',
+        description: 'A software solution and reservation system that makes it simple for users to book an appointment.Can be used for any kind of business such as clinics, salon services and any other industry.',
         link: 'https://urgent-care-checkin-2b5a8d3b5c6d.herokuapp.com/',
         github: 'https://github.com/marjbaut/My-Portfolio'
     },
+    {
+        name: 'E-clinic',
+        description: 'Application where Doctors are able to add patients and edit patients notes.',
+        link: 'https://eclinicproject2.herokuapp.com/',
+        GitHub: 'https://github.com/marjbaut/clinicManagement',
+    },
 ];
 
-export default projectData;
+export default projectData; 
