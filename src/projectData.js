c53c6567ed19af9258190dd903a66acbcd972c2e
Marjorie <marjoriebaut.w@gmail.com>
2023-06-29 12:11:29 -0400

    added data;

--- src/projectData.js
+++ src/projectData.js
@@ -1,22 +1,22 @@
 
 const projectData = [
     {
-        name: 'NoteTaker',
+        name: 'E-clinic',
         description: 'This application will use an Express.js back end and will save and retrieve note data from a JSON file.',
-        link: 'http://dsouzarc.github.io/',
-        image: 'proj1'
+        link: 'https://eclinicproject2.herokuapp.com/',
+        GitHub: 'https://github.com/marjbaut/clinicManagement',
     },
+    // {
+    //     name: 'NoteTaker',
+    //     description: 'This website can be used to organize patient and doctor records in hopitals or clinics.',
+    //     link: 'https://note-taker-mb.herokuapp.com/notes',
+    //     GitHub: 'proj2'
+    // },
     {
-        name: 'NoteTaker',
+        name: 'Appointment Booking System',
         description: 'This website can be used to organize patient and doctor records in hopitals or clinics.',
-        link: 'https://note-taker-mb.herokuapp.com/notes',
-        image: 'proj2'
-    },
-    {
-        name: 'E-CLINIC',
-        description: 'This website can be used to organize patient and doctor records in hopitals or clinics.',
-        link: 'https://github.com/evothinke/eClinic',
-        image: 'This website can be used to organize patient and doctor records in hopitals or clinics.'
+        link: 'https://urgent-care-checkin-2b5a8d3b5c6d.herokuapp.com/',
+        GitHub: 'This website can be used to organize patient and doctor records in hopitals or clinics.'
     },
 ];
 
