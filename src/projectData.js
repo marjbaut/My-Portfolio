6ac2ce7d11e19c241f1b5c053f254937da3c81f8
Marjorie <marjoriebaut.w@gmail.com>
2023-07-05 16:06:16 -0400

    added projects

--- src/projectData.js
+++ src/projectData.js
@@ -11,7 +11,19 @@ const projectData = [
         name: 'E-clinic',
         description: 'Application where Doctors are able to add patients and edit patients notes.',
         link: 'https://eclinicproject2.herokuapp.com/',
-        GitHub: 'https://github.com/marjbaut/clinicManagement',
+        github: 'https://github.com/marjbaut/clinicManagement',
+    },
+    {
+        name: 'NBA tracker',
+        description: 'This website is dedicated to provide up-to-date information about the NBA playoffs using a API. A button that will show the results of the games of the current date. A search button that will show the result that the team score in the last 5 games. On the left-hand side of the website, we provide the latest NBA headlines with links. ',
+        link: 'https://marjbaut.github.io/nba-playoff-tracker-MB/',
+        github: 'https://github.com/marjbaut/nba-playoff-tracker-MB/tree/main',
+    },
+    {
+        name: 'Weather Channel',
+        description: 'Application uses 5 Day Weather Forecast API to retrieve weather data for cities.',
+        link: 'https://marjbaut.github.io/weather-channel/',
+        github: ' https://marjbaut.github.io/weather-channel/',
     },
 ];
 
