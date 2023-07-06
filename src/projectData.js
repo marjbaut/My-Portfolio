005be384da74db43fc51908e9fdc62063bbbf165
marjbaut <128935489+marjbaut@users.noreply.github.com>
2023-07-05 21:24:58 -0400

    Update projectData.js

--- src/projectData.js
+++ src/projectData.js
@@ -9,15 +9,13 @@ const projectData = [
     },
     {
         name: 'E-clinic',
-        description: 'Node.js and Express.js to create a RESTful API.Use Handlebars.js as the templating engine and use MySQL and the Sequelize ORM for the database. Applications where doctors are able to add patients to the system by filling a form, doctors are also able to add notes in the patient profile.
-',
+        description: 'Node.js and Express.js to create a RESTful API.Use Handlebars.js as the templating engine and use MySQL and the Sequelize ORM for the database. Applications where doctors are able to add patients to the system by filling a form, doctors are also able to add notes in the patient profile.',
         link: 'https://eclinicproject2.herokuapp.com/',
         github: 'https://github.com/marjbaut/clinicManagement',
     },
     {
         name: 'NBA tracker',
-        description: 'Application server-side APIs. Client-side storage to store persistent data. API: RapidAPI-NBA. This website is dedicated to provide up-to-date information about the NBA playoffs using an API. It displays real-time game scores of the current date. It also shows results of the last 5 games of the entered team. On the left-hand side of the website, we provide the latest NBA headlines.
-',
+        description: 'Application server-side APIs. Client-side storage to store persistent data. API: RapidAPI-NBA. This website is dedicated to provide up-to-date information about the NBA playoffs using an API. It displays real-time game scores of the current date. It also shows results of the last 5 games of the entered team. On the left-hand side of the website, we provide the latest NBA headlines.',
         link: 'https://marjbaut.github.io/nba-playoff-tracker-MB/',
         github: 'https://github.com/marjbaut/nba-playoff-tracker-MB/tree/main',
     },
