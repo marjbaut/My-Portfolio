f6008aa4e7b7bbcebd3535dd5b862f639776793f
marjbaut <128935489+marjbaut@users.noreply.github.com>
2023-07-05 21:19:52 -0400

    Update projectData.js

--- src/projectData.js
+++ src/projectData.js
@@ -3,19 +3,21 @@ const projectData = [
  
     {
         name: 'Appointment Booking System',
-        description: 'A software solution and reservation system that makes it simple for users to book an appointment.Can be used for any kind of business such as clinics, salon services and any other industry.',
+        description: 'React for the front end, GraphQL, Node.js and Express.js for server and MongoDB and the Mongoose ODM for the database. A software solution and reservation system that makes it simple for users to book an appointment.Can be used for any kind of business such as clinics, salon services and any other industry that might need a booking system.',
         link: 'https://urgent-care-checkin-2b5a8d3b5c6d.herokuapp.com/',
         github: 'https://github.com/marjbaut/My-Portfolio'
     },
     {
         name: 'E-clinic',
-        description: 'Application where Doctors are able to add patients and edit patients notes.',
+        description: 'Node.js and Express.js to create a RESTful API.Use Handlebars.js as the templating engine and use MySQL and the Sequelize ORM for the database. Applications where doctors are able to add patients to the system by filling a form, doctors are also able to add notes in the patient profile.
+',
         link: 'https://eclinicproject2.herokuapp.com/',
         github: 'https://github.com/marjbaut/clinicManagement',
     },
     {
         name: 'NBA tracker',
-        description: 'This website is dedicated to provide up-to-date information about the NBA playoffs using a API. A button that will show the results of the games of the current date. A search button that will show the result that the team score in the last 5 games. On the left-hand side of the website, we provide the latest NBA headlines with links. ',
+        description: 'Application server-side APIs. Client-side storage to store persistent data. API: RapidAPI-NBA. This website is dedicated to provide up-to-date information about the NBA playoffs using an API. It displays real-time game scores of the current date. It also shows results of the last 5 games of the entered team. On the left-hand side of the website, we provide the latest NBA headlines.
+',
         link: 'https://marjbaut.github.io/nba-playoff-tracker-MB/',
         github: 'https://github.com/marjbaut/nba-playoff-tracker-MB/tree/main',
     },
